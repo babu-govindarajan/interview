bb63fa430f5c089097c5df715891b61bdb52bd80
Babu Govindarajan <babu.govindarajan@gmail.com>
2020-06-14 22:40:06 -0700

    Update GreatestCommonDivisor.java

--- GreatestCommonDivisor.java
+++ GreatestCommonDivisor.java
@@ -5,4 +5,22 @@
 
 Calculate GCD = 3
 
+20 - 1, 2, 4, 5, 10, 20
+50 - 1, 2, 5, 10, 25, 50
+130 - 1, 2, 5, 10, 13, 130
 */
+
+
+public class GreatestCommonDivisor {
+ 
+  public static void main(String[] args) {
+    
+    int result = computeGreatestCommonDivisor(3, {3, 9, 15});
+    int result = computeGreatestCommonDivisor(3, {20, 50, 130});
+    
+  }
+  
+  public int computeGreatestCommonDivisor(int n, int [] numbers) {
+    
+  }
+}
